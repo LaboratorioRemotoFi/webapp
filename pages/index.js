76ae269eb5ac614c8b6887d7c8b680c6016d8d46
LupusMaestus <ceballos.equihua@gmail.com>
2021-10-31 17:40:12 -0600

    Runs prettier on all files

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
+import React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
 
 export default function Index() {
   return (

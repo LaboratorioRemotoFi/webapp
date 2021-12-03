ca19a4c8a07f06fe97e1eae0ba54e0a3d16540b4
washedquill8606 <scdo117ds@gmail.com>
2021-12-03 09:44:31 -0600

    little changes

--- src/hooks/studentsReducer.js
+++ src/hooks/studentsReducer.js
@@ -13,6 +13,8 @@ const studentsReducer = (state, action) => {
     // stuff
     case "Logout":
     // stuff
+    default:
+      return state;
   }
 };
 
@@ -22,28 +24,29 @@ const initialState = {
     type: "student", //student - professor
     email: "abc@gmail.com",
     name: "Alex",
-    groupsIds: ["2021-2_1500_2"],
+    groupsIds: [
+      "2021-2_1500_2",
+      "2021-2_1501_3",
+      "2021-2_1502_1"
+    ],
   },
   groups: {
     "2021-2_1500_2": {
       id: "2021-2_1500_2",
       semester: "2021-2",
       subjectId: 1500,
-      groupId: 2,
-      groupNumber: 1,
+      groupNumber: 2,
     },
     "2021-2_1501_3": {
       id: "2021-2_1500_2",
       semester: "2021-2",
       subjectId: 1501,
-      groupId: 3,
-      groupNumber: 1,
+      groupNumber: 3,
     },
     "2021-2_1502_1": {
       id: "2021-2_1500_2",
       semester: "2021-2",
       subjectId: 1502,
-      groupId: 1,
       groupNumber: 1,
     },
   },
@@ -66,40 +69,77 @@ const initialState = {
   },
   practices: {
     "1500-1": {
+      id: "1500-1",
       ip: "256.256.000.000",
-      name: "Plano inclinado",
+      name: "Plano inclinado 1",
+      day: ["05-01-2021","05-01-2021","05-01-2021"],
+      startDate: ["7:00","8:00","9:00"],
+      dueDate: ["9:00","10:00","11:00"],
+
     },
     "1500-2": {
+      id: "1500-2",
       ip: "256.256.000.000",
-      name: "Plano inclinado",
+      name: "Plano inclinado 2",
+      day: ["06-01-2021","06-01-2021","06-01-2021"],
+      startDate: ["7:00","8:00","9:00"],
+      dueDate: ["9:00","10:00","11:00"],
     },
     "1500-3": {
+      id: "1500-3",
       ip: "256.256.000.000",
-      name: "Plano inclinado",
+      name: "Plano inclinado 3",
+      day: ["07-01-2021","07-01-2021","07-01-2021"],
+      startDate: ["7:00","8:00","9:00"],
+      dueDate: ["9:00","10:00","11:00"],
     },
     "1501-1": {
+      id: "1501-1",
       ip: "256.256.000.000",
-      name: "Plano inclinado",
+      name: "Fricción 1",
+      day: ["08-01-2021","08-01-2021","08-01-2021"],
+      startDate: ["7:00","8:00","9:00"],
+      dueDate: ["9:00","10:00","11:00"],
     },
     "1501-2": {
+      id: "1501-2",
       ip: "256.256.000.000",
-      name: "Plano inclinado",
+      name: "Fricción 2",
+      day: ["09-01-2021","09-01-2021","09-01-2021"],
+      startDate: ["7:00","8:00","9:00"],
+      dueDate: ["9:00","10:00","11:00"],
     },
     "1501-3": {
+      id: "1501-3",
       ip: "256.256.000.000",
-      name: "Plano inclinado",
+      name: "Fricción 3",
+      day: ["10-01-2021","10-01-2021","10-01-2021"],
+      startDate: ["7:00","8:00","9:00"],
+      dueDate: ["9:00","10:00","11:00"],
     },
     "1502-1": {
+      id: "1502-1",
       ip: "256.256.000.000",
-      name: "Plano inclinado",
+      name: "Caída libre 1",
+      day: ["11-01-2021","11-01-2021","11-01-2021"],
+      startDate: ["7:00","8:00","9:00"],
+      dueDate: ["9:00","10:00","11:00"],
     },
     "1502-2": {
+      id: "1502-2",
       ip: "256.256.000.000",
-      name: "Plano inclinado",
+      name: "Caída libre 2",
+      day: ["12-01-2021","12-01-2021","12-01-2021"],
+      startDate: ["7:00","8:00","9:00"],
+      dueDate: ["9:00","10:00","11:00"],
     },
     "1502-3": {
+      id: "1502-3",
       ip: "256.256.000.000",
-      name: "Plano inclinado",
+      name: "Caída libre 3",
+      day: ["13-01-2021","13-01-2021","13-01-2021"],
+      startDate: ["7:00","8:00","9:00"],
+      dueDate: ["9:00","10:00","11:00"],
     },
   },
 };

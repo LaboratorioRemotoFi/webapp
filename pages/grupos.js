13cb57f858d1d91c3872fd02d568e6c0d59de51a
washedquill8606 <scdo117ds@gmail.com>
2022-01-28 20:32:18 -0600

    useState in details

--- pages/grupos.js
+++ pages/grupos.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useRouter } from "next/router";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
@@ -18,10 +18,70 @@ import useStoreContext from "../src/hooks/storeContext";
 
 export default function Index() {
   const router = useRouter();
+  const [group, setGroup] = useState(null);
+  //const { groupNumber } = router.query;
+  //const currGroup = groupNumber && currentState?.groups?.[groupNumber];
   const [currentState, currentDispatch] = useStoreContext();
-  const { user, subjects, groups, practices } = currentState ? currentState : 0;
+  const { user, subjects, groups, practices, students } = currentState
+    ? currentState
+    : 0;
 
-  return (
+  return group ? (
+    <>
+      <Layout>
+        <Container maxWidth="false">
+          <Box my={4}>
+            <Typography variant="h8">Grupo {group}</Typography>
+            <Typography variant="h4">Control de prácticas</Typography>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Alumno</TableCell>
+
+                    {Object.values(practices).map((cell) => (
+                      <TableCell alignItems="center" key={cell.name}>
+                        <TableCell
+                          sx={{ borderBottom: "none" }}
+                          component="th"
+                          scope="row"
+                        >
+                          {cell.name}
+                        </TableCell>
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {!currentState
+                    ? "NO DATA"
+                    : Object.values(students).map((row) => (
+                        <TableRow
+                          key={row.name}
+                          sx={{
+                            "&:last-child td, &:last-child th": { border: 0 },
+                          }}
+                        >
+                          <TableCell component="th" scope="row">
+                            {row.name}
+                          </TableCell>
+                          <TableCell>Bloqueado</TableCell>
+                          <TableCell>Bloqueado</TableCell>
+                          <TableCell>Bloqueado</TableCell>
+                          <TableCell>Bloqueado</TableCell>
+                        </TableRow>
+                      ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Box>
+          <Link href="" color="secondary" onClick={() => setGroup(null)}>
+            Volver
+          </Link>
+        </Container>
+      </Layout>
+    </>
+  ) : (
     <>
       <Layout>
         <Container maxWidth="false">
@@ -56,7 +116,11 @@ export default function Index() {
                           </TableCell>
                           <TableCell align="right">{row.groupNumber}</TableCell>
                           <TableCell align="right">
-                            <Link href="/detalles" color="secondary">
+                            <Link
+                              href=""
+                              color="secondary"
+                              onClick={() => setGroup(row.groupNumber)}
+                            >
                               Detalles
                             </Link>
                           </TableCell>

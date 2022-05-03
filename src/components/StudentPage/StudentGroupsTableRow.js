c2a4cb8ca134613b1c7011d3ec5c80ee7a47c03e
Nathaniel Ceballos <31326270+CebEq@users.noreply.github.com>
2022-05-02 20:55:09 -0500

    Changes schedule states and table format (#46)
    * Changes schedule states displayed to student
    
    * Deletes unnecessary endpoint
    
    * Deletes date and duration columns
    
    * Cleans column deletions
    
    * Adds info icon and deletes details

--- src/components/StudentPage/StudentGroupsTableRow.js
+++ src/components/StudentPage/StudentGroupsTableRow.js
@@ -13,13 +13,9 @@ import {
   TableHead,
   TableRow,
 } from "@mui/material";
-import convertDateToSpanishString from "../../utils/timeUtils";
 
 function StudentGroupsTableRow({ group }) {
   const [open, setOpen] = React.useState(false);
-
-  let initialDate;
-  let finalDate;
 
   return (
     <>
@@ -47,52 +43,26 @@ function StudentGroupsTableRow({ group }) {
                 <TableHead>
                   <TableRow>
                     <TableCell width="5%">No.</TableCell>
-                    <TableCell width="20%">Nombre</TableCell>
-                    <TableCell width="10%">Duración</TableCell>
-                    <TableCell width="15%">Fecha de inicio</TableCell>
-                    <TableCell width="15%">Fecha final</TableCell>
-                    <TableCell width="35%">Agendar</TableCell>
+                    <TableCell width="30%">Nombre</TableCell>
+                    <TableCell width="65%">Estado</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {group?.practices.map(
-                    (practiceRow, idx) => (
-                      ((initialDate = convertDateToSpanishString(
-                        practiceRow.startDate
-                      )),
-                      (finalDate = convertDateToSpanishString(
-                        practiceRow.endDate
-                      )),
-                      (initialDate[0] =
-                        initialDate[0].charAt(0).toUpperCase() +
-                        initialDate[0].slice(1)),
-                      (finalDate[0] =
-                        finalDate[0].charAt(0).toUpperCase() +
-                        finalDate[0].slice(1))),
-                      (
-                        <TableRow key={practiceRow.id}>
-                          <TableCell component="th" scope="row">
-                            {idx + 1}
-                          </TableCell>
-                          <TableCell>{practiceRow.name}</TableCell>
-                          <TableCell>{practiceRow.timeFrame} minutos</TableCell>
-                          <TableCell>
-                            {initialDate[0]} a las {initialDate[1]}
-                          </TableCell>
-                          <TableCell>
-                            {finalDate[0]} a las {finalDate[1]}
-                          </TableCell>
-                          <TableCell>
-                            <StudentScheduleLink
-                              practice={practiceRow}
-                              groupId={group.id}
-                              subjectId={group.subjectId}
-                            />
-                          </TableCell>
-                        </TableRow>
-                      )
-                    )
-                  )}
+                  {group?.practices.map((practiceRow, idx) => (
+                    <TableRow key={practiceRow.id}>
+                      <TableCell component="th" scope="row">
+                        {idx + 1}
+                      </TableCell>
+                      <TableCell>{practiceRow.name}</TableCell>
+                      <TableCell>
+                        <StudentScheduleLink
+                          practice={practiceRow}
+                          groupId={group.id}
+                          subjectId={group.subjectId}
+                        />
+                      </TableCell>
+                    </TableRow>
+                  ))}
                 </TableBody>
               </Table>
             </Box>
--- src/components/StudentPage/StudentScheduleLink.js
+++ src/components/StudentPage/StudentScheduleLink.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Button, Typography } from "@mui/material";
+import { Grid, IconButton, Typography } from "@mui/material";
 import PropTypes from "prop-types";
+import InfoIcon from "@mui/icons-material/Info";
 import convertDateToSpanishString from "../../utils/timeUtils";
 import StudentScheduleReservationModal from "./StudentScheduleReservationModal.js";
 import StudentScheduleDetails from "./StudentScheduleDetails.js";
@@ -11,9 +12,14 @@ function StudentScheduleLink(props) {
   const { practice, groupId, subjectId } = props;
   const startDate = practice.startDate;
   const endDate = practice.endDate;
-  const timeFrame = practice.timeFrame;
   const currentStudentScheduleTimestamp =
     practice?.currentStudentSchedule?.timestamp;
+
+  let scheduleStatus = practice?.currentStudentSchedule?.status;
+
+  if (typeof scheduleStatus === "undefined") {
+    scheduleStatus = "NOT SCHEDULED";
+  }
 
   const [isModalOpen, setIsModalOpen] = React.useState(false);
   const openModal = () => setIsModalOpen(true);
@@ -22,36 +28,36 @@ function StudentScheduleLink(props) {
   // Days * 24 hours * 60 minutes * 60 seconds * 1000 milliseconds
   const reserveTime = 7 * 24 * 60 * 60 * 1000;
 
-  let state;
-
-  const hasSchedule = currentStudentScheduleTimestamp > 0;
   const scheduleString = convertDateToSpanishString(
     currentStudentScheduleTimestamp
   );
   const schedulingDate = convertDateToSpanishString(startDate - reserveTime);
-  const availableDateStart = convertDateToSpanishString(startDate);
-  const availableDateEnd = convertDateToSpanishString(endDate);
 
-  if (currDate > endDate) {
-    state = "Expired";
+  let state;
+
+  if (scheduleStatus?.localeCompare("FINISHED") === 0) {
+    state = "Finished";
+  } else if (scheduleStatus?.localeCompare("STARTED") === 0) {
+    state = "Started";
   } else if (
-    hasSchedule &&
-    currDate > currentStudentScheduleTimestamp &&
-    currDate < endDate
+    scheduleStatus?.localeCompare("SCHEDULED") === 0 &&
+    currDate < currentStudentScheduleTimestamp
   ) {
-    state = "Late reschedule";
+    state = "Scheduled";
   } else if (
-    hasSchedule &&
-    currDate < currentStudentScheduleTimestamp &&
+    scheduleStatus?.localeCompare("SCHEDULED") === 0 &&
+    currDate > currentStudentScheduleTimestamp &&
     currDate < endDate
   ) {
     state = "Reschedule";
   } else if (
-    !hasSchedule &&
+    scheduleStatus?.localeCompare("NOT SCHEDULED") === 0 &&
     currDate < endDate &&
     currDate >= startDate - reserveTime
   ) {
     state = "Available";
+  } else if (currDate > endDate) {
+    state = "Expired";
   } else if (currDate < startDate - reserveTime) {
     state = "Not available";
   }
@@ -59,186 +65,118 @@ function StudentScheduleLink(props) {
   let component;
 
   switch (state) {
-    case "Expired":
-      // Change for !done
-      if (!hasSchedule) {
-        component = (
-          <>
-            <StudentScheduleDetails
-              header={
-                <Typography variant="inherit" color="red" fontWeight="bold">
-                  Expirada
-                </Typography>
-              }
-              details={
-                <>
-                  <Typography variant="inherit">No fue agendada.</Typography>
-                  <Typography variant="inherit" fontStyle="italic">
-                    Estuvo disponible para realizar del {availableDateStart[0]}{" "}
-                    a las {availableDateStart[1]} al {availableDateEnd[0]} a las{" "}
-                    {availableDateEnd[1]}.
-                  </Typography>
-                  <Typography variant="inherit">
-                    Duración: {timeFrame} minutos.
-                  </Typography>
-                </>
-              }
-            />
-          </>
-        );
-      } else {
-        component = (
-          <>
-            <StudentScheduleDetails
-              header={
-                <Typography variant="inherit" fontWeight="bold">
-                  Terminada
-                </Typography>
-              }
-              details={
-                <>
-                  <Typography variant="inherit">
-                    Fue agendada para el {scheduleString[0]} a las{" "}
-                    {scheduleString[1]} y se realizó ([detalles])/no se realizó.
-                  </Typography>
-                  <Typography variant="inherit" fontStyle="italic">
-                    Estuvo disponible para realizar del {availableDateStart[0]}{" "}
-                    a las {availableDateStart[1]} al {availableDateEnd[0]} a las{" "}
-                    {availableDateEnd[1]}.
-                  </Typography>
-                  <Typography variant="inherit">
-                    Duración: {timeFrame} minutos.
-                  </Typography>
-                </>
-              }
-            />
-          </>
-        );
-      }
-      break;
-    // ADD CHECK FOR DONE/NOT DONE
-    case "Late reschedule":
+    case "Finished":
       component = (
-        <>
-          <StudentScheduleDetails
-            header={
-              <Typography variant="inherit" color="red" fontWeight="bold">
-                Expirada (
-                <Button
-                  sx={{
-                    minHeight: 0,
-                    minWidth: 0,
-                    padding: 0,
-                    textTransform: "none",
-                  }}
-                  color="secondary"
-                  fontWeight="bold"
-                  onClick={openModal}
-                >
-                  Reagendar
-                </Button>
-                )
-              </Typography>
-            }
-            details={
-              <>
-                <Typography variant="inherit">
-                  Fue agendada para el {scheduleString[0]} a las{" "}
-                  {scheduleString[1]}.
-                </Typography>
-                <Typography variant="inherit" fontStyle="italic">
-                  Está disponible para realizar del {availableDateStart[0]} a
-                  las {availableDateStart[1]} al {availableDateEnd[0]} a las{" "}
-                  {availableDateEnd[1]}.
-                </Typography>
-                <Typography variant="inherit">
-                  Duración: {timeFrame} minutos.
-                </Typography>
-              </>
-            }
-          />
-        </>
+        <Grid container direction="row" alignItems="center">
+          <Grid item>
+            <Typography variant="inherit" fontWeight="bold">
+              Terminada
+            </Typography>
+          </Grid>
+          <Grid item>
+            <IconButton aria-label="info" color="secondary" size="small">
+              <InfoIcon fontSize="inherit" />
+            </IconButton>
+          </Grid>
+        </Grid>
+      );
+      break;
+    case "Started":
+      component = (
+        <Grid container direction="row" alignItems="center">
+          <Grid item>
+            <Typography variant="inherit" fontWeight="bold">
+              Empezada
+            </Typography>
+          </Grid>
+          <Grid item>
+            <IconButton aria-label="info" color="secondary" size="small">
+              <InfoIcon fontSize="inherit" />
+            </IconButton>
+          </Grid>
+        </Grid>
+      );
+      break;
+    case "Scheduled":
+      component = (
+        <Grid container direction="row" alignItems="center">
+          <Grid item>
+            <Typography variant="inherit" fontWeight="bold">
+              Agendada
+            </Typography>
+          </Grid>
+          <Grid item>
+            <IconButton aria-label="info" color="secondary" size="small">
+              <InfoIcon fontSize="inherit" />
+            </IconButton>
+          </Grid>
+        </Grid>
       );
       break;
     case "Reschedule":
       component = (
-        <>
-          <StudentScheduleDetails
-            header={
-              <Typography variant="inherit" fontWeight="bold">
-                Agendada (
-                <Button
-                  sx={{
-                    minHeight: 0,
-                    minWidth: 0,
-                    padding: 0,
-                    textTransform: "none",
-                  }}
-                  color="secondary"
-                  fontWeight="bold"
-                  onClick={openModal}
-                >
-                  Reagendar
-                </Button>
-                )
-              </Typography>
-            }
-            details={
-              <>
-                <Typography variant="inherit">
-                  Fue agendada para el {scheduleString[0]} a las{" "}
-                  {scheduleString[1]}.
-                </Typography>
-                <Typography variant="inherit" fontStyle="italic">
-                  Está disponible para realizar del {availableDateStart[0]} a
-                  las {availableDateStart[1]} al {availableDateEnd[0]} a las{" "}
-                  {availableDateEnd[1]}.
-                </Typography>
-                <Typography variant="inherit">
-                  Duración: {timeFrame} minutos.
-                </Typography>
-              </>
-            }
-          />
-        </>
+        <Grid container direction="row" alignItems="center">
+          <Grid item>
+            <Typography variant="inherit" color="red" fontWeight="bold">
+              Por agendar
+            </Typography>
+          </Grid>
+          <Grid item>
+            <IconButton aria-label="info" color="secondary" size="small">
+              <InfoIcon fontSize="inherit" />
+            </IconButton>
+          </Grid>
+        </Grid>
       );
       break;
     case "Available":
       component = (
-        <>
-          <StudentScheduleDetails
-            header={
-              <>
-                <Button
-                  sx={{
-                    minHeight: 0,
-                    minWidth: 0,
-                    padding: 0,
-                    textTransform: "none",
-                  }}
-                  color="secondary"
-                  fontWeight="bold"
-                  onClick={openModal}
-                >
-                  Agendar
-                </Button>
-              </>
-            }
-            details={
-              <>
-                <Typography variant="inherit" fontStyle="italic">
-                  Está disponible para realizar del {availableDateStart[0]} a
-                  las {availableDateStart[1]} al {availableDateEnd[0]} a las{" "}
-                  {availableDateEnd[1]}.
-                </Typography>
-                <Typography variant="inherit">
-                  Duración: {timeFrame} minutos.
-                </Typography>
-              </>
-            }
-          />
-        </>
+        <Grid container direction="row" alignItems="center">
+          <Grid item>
+            <Typography variant="inherit" fontWeight="bold">
+              Por agendar
+            </Typography>
+          </Grid>
+          <Grid item>
+            <IconButton aria-label="info" color="secondary" size="small">
+              <InfoIcon fontSize="inherit" />
+            </IconButton>
+          </Grid>
+        </Grid>
       );
+      break;
+    case "Expired":
+      if (!scheduleStatus?.localeCompare("NOT SCHEDULED")) {
+        component = (
+          <Grid container direction="row" alignItems="center">
+            <Grid item>
+              <Typography variant="inherit" color="red" fontWeight="bold">
+                Expirada
+              </Typography>
+            </Grid>
+            <Grid item>
+              <IconButton aria-label="info" color="secondary" size="small">
+                <InfoIcon fontSize="inherit" />
+              </IconButton>
+            </Grid>
+          </Grid>
+        );
+      } else {
+        component = (
+          <Grid container direction="row" alignItems="center">
+            <Grid item>
+              <Typography variant="inherit" color="red" fontWeight="bold">
+                Expirada
+              </Typography>
+            </Grid>
+            <Grid item>
+              <IconButton aria-label="info" color="secondary" size="small">
+                <InfoIcon fontSize="inherit" />
+              </IconButton>
+            </Grid>
+          </Grid>
+        );
+      }
       break;
     case "Not available":
       component = (
@@ -255,11 +193,6 @@ function StudentScheduleLink(props) {
                   Estará disponible para agendar a partir del{" "}
                   {schedulingDate[0]} a las {schedulingDate[1]}.
                 </Typography>
-                <Typography variant="inherit" fontStyle="italic">
-                  Estará disponible para realizar del {availableDateStart[0]} a
-                  las {availableDateStart[1]} al {availableDateEnd[0]} a las{" "}
-                  {availableDateEnd[1]}.
-                </Typography>
               </>
             }
           />
@@ -272,7 +205,6 @@ function StudentScheduleLink(props) {
           No disponible
         </Typography>
       );
-      break;
   }
 
   return (
